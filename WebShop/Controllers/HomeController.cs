b53d0974fe6118df949d18293b6cdd300f9aa0bf
JohanSvantesson <55137674+JohanSvantesson@users.noreply.github.com>
2021-02-15 15:35:08 +0100

    crud categories

--- WebShop/Controllers/HomeController.cs
+++ WebShop/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using System.Linq;
@@ -27,6 +28,58 @@ namespace WebShop.Controllers
                 Id = dbCategory.Id,
                 Name = dbCategory.Name
             }).ToList();
+            return View(viewModel);
+        }
+
+        
+        [Authorize(Roles = "Admin, Product Manager")]
+        public IActionResult Edit(int Id)
+        {
+            var viewModel = new HomeEditViewModel();
+            var dbcat = _dbContext.ProductCategory.First(p => p.Id == Id);
+            viewModel.Id = dbcat.Id;
+            viewModel.Name = dbcat.Name;
+
+            return View(viewModel);
+        }
+        
+        [Authorize(Roles = "Admin, Product Manager")]
+        [HttpPost]
+        public IActionResult Edit(int Id, HomeEditViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbcat = _dbContext.ProductCategory.First(p => p.Id == Id);
+                dbcat.Id = viewModel.Id;
+                dbcat.Name = viewModel.Name;
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(viewModel);
+        }
+        [Authorize(Roles = "Admin, Product Manager")]
+        public IActionResult NewCategory()
+        {
+            var viewModel = new HomeNewCategoryViewModel();
+            
+
+            return View(viewModel);
+        }
+        [Authorize(Roles = "Admin, Product Manager")]
+        [HttpPost]
+        public IActionResult NewCategory(HomeNewCategoryViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbcat = new ProductCategory();
+                _dbContext.Add(dbcat);
+                dbcat.Name = viewModel.Name;
+
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
             return View(viewModel);
         }
 
---
+++ WebShop/ViewModels/HomeEditViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.ViewModels
+{
+    public class HomeEditViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [MinLength(1)]
+        public string Name { get; set; }
+    }
+}
